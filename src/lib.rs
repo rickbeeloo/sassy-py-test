edb5e9c6105f1fe41ecba5dbc04b2674dc353031
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2025-04-27 16:19:34 +0200

    simplify IUPAC initialization

--- src/lib.rs
+++ src/lib.rs
@@ -7,26 +7,35 @@ use std::arch::x86_64::*;
 pub const IUPAC_CODE: [u8; 32] = {
     let mut t = [0u8; 32];
     // Standard bases
-    t[b'A' as usize & 0x1F] = 0b1000; t[b'a' as usize & 0x1F] = 0b1000; // A
-    t[b'C' as usize & 0x1F] = 0b0001; t[b'c' as usize & 0x1F] = 0b0001; // C
-    t[b'G' as usize & 0x1F] = 0b0010; t[b'g' as usize & 0x1F] = 0b0010; // G
-    t[b'T' as usize & 0x1F] = 0b0100; t[b't' as usize & 0x1F] = 0b0100; // T
-    t[b'N' as usize & 0x1F] = 0b1111; t[b'n' as usize & 0x1F] = 0b1111; // Any
+    // Map ACGT -> [0,1,3,2], like packed_seq does.
+    const A: u8 = 1<<0;
+    const C: u8 = 1<<1;
+    const G: u8 = 1<<3;
+    const T: u8 = 1<<2;
+
+    // Map common chars.
+    // Lower case has the same last 5 bits as upper case.
+    // (Thanks ASCII :)
+    t[b'A' as usize & 0x1F] = A;
+    t[b'C' as usize & 0x1F] = C;
+    t[b'G' as usize & 0x1F] = G;
+    t[b'T' as usize & 0x1F] = T;
+    t[b'N' as usize & 0x1F] = A|C|T|G;
     
     // IUPAC ambiguity codes
-    t[b'R' as usize & 0x1F] = 0b1010; t[b'r' as usize & 0x1F] = 0b1010; // A or G
-    t[b'Y' as usize & 0x1F] = 0b0101; t[b'y' as usize & 0x1F] = 0b0101; // C or T
-    t[b'S' as usize & 0x1F] = 0b0011; t[b's' as usize & 0x1F] = 0b0011; // G or C
-    t[b'W' as usize & 0x1F] = 0b1100; t[b'w' as usize & 0x1F] = 0b1100; // A or T
-    t[b'K' as usize & 0x1F] = 0b0110; t[b'k' as usize & 0x1F] = 0b0110; // G or T
-    t[b'M' as usize & 0x1F] = 0b1001; t[b'm' as usize & 0x1F] = 0b1001; // A or C
-    t[b'B' as usize & 0x1F] = 0b0111; t[b'b' as usize & 0x1F] = 0b0111; // C or G or T
-    t[b'D' as usize & 0x1F] = 0b1110; t[b'd' as usize & 0x1F] = 0b1110; // A or G or T
-    t[b'H' as usize & 0x1F] = 0b1101; t[b'h' as usize & 0x1F] = 0b1101; // A or C or T
-    t[b'V' as usize & 0x1F] = 0b1011; t[b'v' as usize & 0x1F] = 0b1011; // A or C or G
+    t[b'R' as usize & 0x1F] = A|G;
+    t[b'Y' as usize & 0x1F] = C|T;
+    t[b'S' as usize & 0x1F] = G|C;
+    t[b'W' as usize & 0x1F] = A|T;
+    t[b'K' as usize & 0x1F] = G|T;
+    t[b'M' as usize & 0x1F] = A|C;
+    t[b'B' as usize & 0x1F] = C|G|T;
+    t[b'D' as usize & 0x1F] = A|G|T;
+    t[b'H' as usize & 0x1F] = A|C|T;
+    t[b'V' as usize & 0x1F] = A|C|G;
     
     // Gap/unknown
-    t[b'X' as usize & 0x1F] = 0b0000; t[b'x' as usize & 0x1F] = 0b0000;
+    t[b'X' as usize & 0x1F] = 0;
     
     t
 };
