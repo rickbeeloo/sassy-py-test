5c3ddc733a38adfcb4a013dda38f5f3db24620cd
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2025-05-01 22:04:52 +0200

    Add multithreading to main binary

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use std::path::PathBuf;
+#![feature(let_chains)]
+use std::{path::PathBuf, sync::Mutex};
 
 use clap::Parser;
 use sassy::{
@@ -35,24 +36,42 @@ fn main() {
 
     let query = args.query.as_bytes();
 
-    let mut reader = needletail::parse_fastx_file(args.path).unwrap();
+    let reader = Mutex::new(needletail::parse_fastx_file(args.path).unwrap());
+    let write_lock = Mutex::new(());
 
-    while let Some(record) = reader.next() {
-        let record = record.unwrap();
-        let id = str::from_utf8(record.id()).unwrap();
-        let text = &record.seq();
-        // eprintln!("Searching {id}");
-        let matches = match args.alphabet {
-            Alphabet::Ascii => search::<Ascii<true>>(query, text, args.k),
-            Alphabet::Dna => search::<Dna>(query, text, args.k),
-            Alphabet::Iupac => search::<Iupac>(query, text, args.k),
-        };
-        for m in matches {
-            let cost = m.0;
-            let start = m.1.first().unwrap().1;
-            let end = m.1.last().unwrap().1;
-            let slice = str::from_utf8(&text[start..end]).unwrap();
-            println!("{id}\t{cost}\t{start}\t{end}\t{slice}");
+    // Launch as many threads as there are cores.
+    let num_threads = num_cpus::get();
+    std::thread::scope(|scope| {
+        for _ in 0..num_threads {
+            scope.spawn(|| {
+                while let Ok(mut guard) = reader.lock()
+                    && let Some(record) = guard.next()
+                {
+                    let record = record.unwrap();
+                    let id = String::from_utf8(record.id().to_vec()).unwrap();
+                    let text = &record.seq().into_owned();
+
+                    // Release the lock.
+                    drop(record);
+                    drop(guard);
+
+                    // eprintln!("Searching {id}");
+                    let matches = match args.alphabet {
+                        Alphabet::Ascii => search::<Ascii<true>>(query, text, args.k),
+                        Alphabet::Dna => search::<Dna>(query, text, args.k),
+                        Alphabet::Iupac => search::<Iupac>(query, text, args.k),
+                    };
+
+                    let _write_lock = write_lock.lock().unwrap();
+                    for m in matches {
+                        let cost = m.0;
+                        let start = m.1.first().unwrap().1;
+                        let end = m.1.last().unwrap().1;
+                        let slice = str::from_utf8(&text[start..end]).unwrap();
+                        println!("{id}\t{cost}\t{start}\t{end}\t{slice}");
+                    }
+                }
+            });
         }
-    }
+    });
 }
